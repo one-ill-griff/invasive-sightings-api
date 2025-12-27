ff167a4d1aa45bce9d1f07f76cb112645c837d55
griffinoneill <griffin.a.oneill@gmail.com>
2025-12-26 22:04:13 -0500

    Update db.js

--- src/db.js
+++ src/db.js
@@ -6,7 +6,10 @@ if (!process.env.DATABASE_URL) {
   throw new Error("DATABASE_URL is not set");
 }
 
+console.log("DB: starting pool with SSL rejectUnauthorized=false");
+
 export const pool = new Pool({
   connectionString: process.env.DATABASE_URL,
   ssl: { rejectUnauthorized: false }
 });
+
